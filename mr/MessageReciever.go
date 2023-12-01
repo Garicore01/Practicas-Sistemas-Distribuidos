239b7663c896e91dad2780395764c70077027506
849183 <849183@unizar.es>
2023-12-01 11:22:34 +0100

    Raft Working

---
+++ mr/MessageReciever.go
@@ -0,0 +1,28 @@
+package mr
+
+import (
+	"practica2/ms"
+	"practica2/gf"
+	"practica2/ra"
+)
+
+type Update struct {
+	Text string 
+}
+
+
+
+func ReceiveMessage(msgs *ms.MessageSystem, File string, reqChan chan ra.Request,repChan chan ra.Reply){
+	for {
+		message:= msgs.Receive()
+		// Establecemos los posibles casos del mensaje, para enviar por el canal correspondiente el mensaje.
+		switch messageType := message.(type) {
+		case ra.Request:
+			reqChan <- messageType
+		case ra.Reply:
+			repChan <- messageType
+		case Update:
+			gf.EscribirFichero(File, messageType.Text)
+		}
+	}
+}		
