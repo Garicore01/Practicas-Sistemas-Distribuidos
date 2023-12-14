fd5ccaabee6d45a1c0add00ecc2c36a714ec5a2d
849183 <849183@unizar.es>
2023-12-14 18:46:21 +0100

    I think that this is working

--- Practica5/kb.sh
+++ Practica5/kb.sh
@@ -28,8 +28,8 @@ echo -e "\nLanzando Kubernetes"
 
 kubectl apply -f service_go.json
 kubectl apply -f statefulset_go.json
-sleep 4
-kubectl exec -ti raft-0 -- nslookup raft-0.raft.default.svc.cluster.local
+sleep 10
+kubectl exec -ti raft-0 -- nslookup raft-0.raft.default.svc.cluster.local 
 kubectl exec -ti raft-0 -- nslookup raft-1.raft.default.svc.cluster.local
 kubectl exec -ti raft-0 -- nslookup raft-2.raft.default.svc.cluster.local
 kubectl apply -f pod_go.json
--- Practica5/matar-cliente.sh
+++ Practica5/matar-cliente.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 sudo kubectl delete pod client
 sudo kubectl apply -f pod_go.json
-kubectl apply -f service_go.json
-kubectl apply -f statefulset_go.json
 sleep 4
 kubectl exec -ti raft-0 -- nslookup raft-0.raft.default.svc.cluster.local
 kubectl exec -ti raft-0 -- nslookup raft-1.raft.default.svc.cluster.local
